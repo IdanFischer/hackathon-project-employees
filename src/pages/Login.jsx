3ddbb74d96b7458379126661ef8ce5afdd5d83cf
Idan Fischer <Idanfischer5@gmail.com>
2023-09-30 11:59:22 -0400

    cleaned up repo and started login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function Login() {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col sm="12" md="6">
+          </Col>
+          <Col sm="12" md="6">
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
